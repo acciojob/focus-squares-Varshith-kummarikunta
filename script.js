3d3766f7678f45e9f6ecf987ffa1cced41cc6db5
Varshith-kummarikunta <varshithkummarikunta@gmail.com>
2025-09-02 17:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+		for (let sq of squares) {
+			sq.addEventListener("mouseover", (e) => addBgColor(e));
+			sq.addEventListener("mouseout", () => addDefaultBgColor());
+		}
+
+		function addBgColor(e) {
+			const hoveredId = e.target.id;
+			for (let sq of squares) {
+				if (sq.id !== hoveredId) {
+					sq.style.backgroundColor = "red";
+				}
+			}
+		}
+
+		function addDefaultBgColor() {
+			for (let sq of squares) {
+				sq.style.backgroundColor = "#E6E6FA";
+			}
+		}
